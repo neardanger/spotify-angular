b33d9dd9c877306d97915fc39339a843680f0a88
Neardanger <steven.magadan@hotmail.com>
2016-08-18 11:58:52 -0700

    successfully started angular2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import {NavbarComponent} from './components/navbar/navbar.component';
 
 @Component({
+    moduleId:module.id,
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app.component.html',
+    directives:[NavbarComponent]
 })
 export class AppComponent { }
