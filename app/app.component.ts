1b8c63a917a2567ad25811bd33be71ac265a78b7
Neardanger <steven.magadan@hotmail.com>
2016-08-24 15:13:35 -0700

    added router directives

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from './components/navbar/navbar.component';
+import{ROUTER_DIRECTIVES} from '@angular/router'
 
 @Component({
     moduleId:module.id,
