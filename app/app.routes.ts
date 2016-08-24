49eba2b16473faa6a4a5b6e59c60b83084557990
Neardanger <steven.magadan@hotmail.com>
2016-08-24 15:07:17 -0700

    added main.ts module

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,1 +1,13 @@
 import{provideRouter, RouterConfig} from '@angular/router'
+import{SearchComponent} from  './components/search/search.component.ts'
+import{AboutComponent} from './components/about/about.component.ts'
+
+const routes: RouterConfig=[
+    {path:'',component:'SearchComponent'},
+    {path:'about',component:'AboutComponent'}
+
+]
+
+export const appRouterProviders=[
+    provideRouter(routes)
+]
