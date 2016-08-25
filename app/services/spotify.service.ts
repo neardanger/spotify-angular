e374f440b31626fae86edabcbdfca8d5f5c0e51f
Neardanger <steven.magadan@hotmail.com>
2016-08-25 13:21:00 -0700

    added music service

--- app/services/spotify.service.ts
+++ app/services/spotify.service.ts
@@ -1,3 +1,19 @@
 import {Injectable} from '@angular/core'
 import{Http,Headers} from '@angular/http'
-import 'rxjs/operator/add/'
+import 'rxjs/add/operator/map';
+
+@Injectable()
+
+
+export class SpotifyService{
+    private searchUrl: string;
+
+    constructor(private_http:Http){
+
+    }
+    
+    searchMusic(){
+        
+    }
+}
+
